cf96a50653a7785643d7179d9e46786cd0718a3d
Thais Faria <thhafaria@gmail.com>
2022-06-13 18:13:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# robotron-2000
+
+O projeto robotron 2000 faz parte do curso Manipulando o Dom na Alura onde aprendemos como fazer cálculos dinâmicos. Pude praticar forEach, funções e arrays. Meu código não ficou totalmente igual ao do professor, porque no dele tinha um erro onde só somava mesmo tirando as peças e aceitava peças menores que 0.
---
+++ main.js
@@ -0,0 +1,73 @@
+const controle = document.querySelectorAll('[data-controle]')
+const estatistica = document.querySelectorAll('[data-estatistica]')
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+controle.forEach ( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        ManipulaDados(evento.target.dataset.controle, evento.target.parentNode, evento.target.dataset.peca)
+    })
+})
+
+function ManipulaDados(dataControle, parent, peca){
+    const contador = parent.querySelector('[data-controleContador]')
+    if(dataControle === "-"){
+        if(contador.value > 0){
+            contador.value = Number(contador.value) - 1
+        ManipulaEstatisticas(peca, dataControle)
+        } else{
+            alert('equipamentos menores que 0 não é possivel')
+            
+        }
+        }
+    else {
+        contador.value = Number(contador.value) + 1
+        ManipulaEstatisticas(peca, dataControle)
+    }
+}
+
+function ManipulaEstatisticas(peca, dataControle){
+
+    if (dataControle === "+") {
+        estatistica.forEach( (elemento) => {
+          elemento.textContent = Number(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+        })
+      } else {
+        estatistica.forEach( (elemento) => {
+          elemento.textContent = Number(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+        })
+      }
+}
+    
+
+
